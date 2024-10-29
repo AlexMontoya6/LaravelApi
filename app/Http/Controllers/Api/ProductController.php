f0b21c803ec404949aa1641ee160d2f1916b64e9
monty <alexmonty6@gmail.com>
2024-10-29 16:02:31 +0100

    "Added ProductResource import and usage in ProductController's index method"

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::with('category')->get();
+        
+        return ProductResource::collection($products);
     }
 }

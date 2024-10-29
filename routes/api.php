6cd9c46a7aecdb53fe541566232f8008918230f8
monty <alexmonty6@gmail.com>
2024-10-29 17:00:56 +0100

    "Refactor category routes to use apiResource, comment out individual route definitions"

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('categories/{category}', [CategoryController::class, 'show']);
-Route::get('list/categories', [CategoryController::class, 'list'] );
-Route::post('categories', [CategoryController::class, 'store']);
 Route::get('products', [ProductController::class, 'index']);
-Route::put('categories/{category}', [CategoryController::class, 'update']);
-Route::delete('categories/{category}', [CategoryController::class, 'delete']);
+// Route::get('categories', [CategoryController::class, 'index']);
+// Route::get('categories/{category}', [CategoryController::class, 'show']);
+// Route::get('list/categories', [CategoryController::class, 'list']);
+// Route::post('categories', [CategoryController::class, 'store']);
+// Route::put('categories/{category}', [CategoryController::class, 'update']);
+// Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+Route::apiResource('categories', CategoryController::class);
